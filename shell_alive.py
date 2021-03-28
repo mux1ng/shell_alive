d2e6cade7915f9d306b96678a64bb5a996e63106
mux1ng <49070782+mux1ng@users.noreply.github.com>
2021-03-29 00:24:20 +0800

    Update shell_alive.py

--- shell_alive.py
+++ shell_alive.py
@@ -11,10 +11,22 @@ def readDB(exectCmd="select * from shell", db_path="data.db"):
 	rows = cursor.fetchall()
 	return rows
 
-def connectTest(url):
+def getProxy(proxyType, host, port):
+	if proxyType != "NO_PROXY":
+		proxyType = proxyType.lower()
+		proxyType = "socks5" if proxyType == "socks" else proxyType
+		proxies = {
+			"http": "{}://{}:{}".format(proxyType, host, port),
+			"https": "{}://{}:{}".format(proxyType, host, port)
+		}
+		return proxies
+	else:
+		return None
+
+def connectTest(url, proxies=""):
 	headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36"}
 	try:
-		r = requests.get(url, headers=headers, timeout=3)
+		r = requests.get(url, headers=headers, timeout=3, proxies=proxies)
 		if r.status_code == 200:
 			return True
 		else:
@@ -25,7 +37,8 @@ def connectTest(url):
 def main():
 	rows = readDB()
 	for row in rows:
-		if(connectTest(url=row[1])):
+		proxies = getProxy(proxyType=row[12], host=row[13], port=row[14])
+		if(connectTest(url=row[1], proxies=proxies)):
 			print(row[1])
 
 if __name__ == '__main__':
